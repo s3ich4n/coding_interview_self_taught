b3f2c2b156abbcfaefc2bc1bfe86756195efc1a3
Seongeun Yu <s3ich4n@gmail.com>
2022-12-15 05:37:18 +0900

    feat: 반복문을 사용한 후위순회 로직 추가

--- codes/pt4/14_tree/02_traversal_iterative.py
+++ codes/pt4/14_tree/02_traversal_iterative.py
@@ -50,6 +50,12 @@ def inorder(node):
 
 
 def postorder(node):
+    """ 후위순회, 메모리 두배로 쓰기
+
+    traversal을 기록하고 그거대로 찍기
+
+    훨씬 직관적이긴 함
+    """
     stack = []
     traversal = []
 
@@ -68,9 +74,51 @@ def postorder(node):
         print(current_node.val, end=" ")
 
 
+def postorder2(node):
+    """ 후위순회, 메모리 두배로 쓰고 루프 한번만에 끝내기
+
+    알면 똑똑하다하고 감탄하고 쓸 수 있음.
+
+    https://www.geeksforgeeks.org/iterative-postorder-traversal-using-stack/
+    """
+    stack = []
+    current_node = node
+
+    while True:
+        while current_node:
+            # 후에 있을 right 순회때문에 두번 넣는다.
+            stack.append(current_node)
+            stack.append(current_node)
+            current_node = current_node.left
+
+        if len(stack) == 0:
+            return
+
+        current_node = stack.pop()
+
+        # 우측 순회를 위한 로직.
+        # 1.
+        #   두번 넣은 이유
+        #   한번 current node와 같으면 right쪽을 순회하고
+        #   두번째는 자신의 값을 출력하고 None으로 바꿔서 pop 후
+        #   스택에 쌓은대로 가기 위함이다
+        #
+        # 2.
+        #   len(stack) > 0은
+        #   stack[-1]이 IndexError 뜨는 맨 마지막 순회를 잡기 위함이다
+        if (len(stack) > 0 and stack[-1] == current_node):
+            current_node = current_node.right
+
+        else:
+            print(current_node.val, end=" ")
+            current_node = None
+
+
 preorder(root)
 print()
 inorder(root)
 print()
 postorder(root)
+print()
+postorder2(root)
 print()
